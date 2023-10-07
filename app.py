dc5388e7b7918053262812803203fe10613c91a5
MurataAoi <kc9185910@gmail.com>
2023-10-07 18:10:23 +0900

    4

--- app.py
+++ app.py
@@ -46,7 +46,6 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     reply_message = chat_completion(event.message.text)
-    text = event.message.text
 
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
